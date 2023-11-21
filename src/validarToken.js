b24b80a2424e8138e1a340bb061de92e404a5b05
kyomi <104471832+kyomi-dev@users.noreply.github.com>
2023-11-21 11:45:51 -0300

    refactor(auth): A função 'validarToken' é um middleware agora

--- src/validarToken.js
+++ src/validarToken.js
@@ -1,20 +1,31 @@
+const pool = require("./conexao");
 const { secret } = require("./segredo");
 const jwt = require("jsonwebtoken");
 
 
 const validarToken = async (req, res, next) => {
-    const token = req.headers.authorization;
-
-    if (!token) {
-        return res.status(401).json({ mensagem: "Token não foi enviado na requisição." });
-    }
-
     try {
-        const tokenValidado = await jwt.verify(token, secret);
-        req.usuario = tokenValidado;
+        const { authorization } = req.headers;
+        const token = authorization.split(" ")[1];
+
+        if (!token) {
+            return res.status(401).json({ mensagem: "Para acessar este recurso, um token de autenticação válido deve ser enviado" });
+        }
+
+        const tokenVerificado = jwt.verify(token, secret);
+        const { id } = tokenVerificado;
+
+        const usuario = await pool.query("SELECT * FROM usuarios WHERE id = $1", [id]);
+
+        if (usuario.rows.length === 0) {
+            return res.status(401).json({ mensagem: "Usuário não encontrado." });
+        }
+
+        req.usuario = usuario.rows[0];
+        next();
     } catch (error) {
         return res.status(401).json({ mensagem: "Token inválido." });
     }
-}
+};
 
 module.exports = validarToken;
