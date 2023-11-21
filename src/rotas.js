bfe1c7a6abfa675519aaf785bd4f3d1b78695a49
kyomi <104471832+kyomi-dev@users.noreply.github.com>
2023-11-21 11:52:17 -0300

    feat(routes): Cria rota para pegar dados do usuário logado

--- src/rotas.js
+++ src/rotas.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { cadastrarUsuario, login } = require("./controllers/usuarios");
-const validarLogin = require("./validarLogin");
+const { cadastrarUsuario, login, getDadosUsuario } = require("./controllers/usuarios");
+const { validarLogin } = require("./validarLogin");
+const validarToken = require("./validarToken");
 const rotas = express();
 
 
 rotas.post("/usuario", cadastrarUsuario);
 rotas.post("/login", validarLogin, login);
+rotas.get("/usuario", validarToken, getDadosUsuario)
+
 
 module.exports = rotas;
