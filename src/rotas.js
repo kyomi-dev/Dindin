a5ff2860e3d6f31999cfd31102a8a19bba87a32a
kyomi <104471832+kyomi-dev@users.noreply.github.com>
2023-11-22 07:32:50 -0300

    feat(routes): Adiciona endpoint para edição de informações do usuário

--- src/rotas.js
+++ src/rotas.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { cadastrarUsuario, login, getDadosUsuario, getListarCategorias, listarTransacoes, detalharTransacao } = require("./controllers/usuarios");
+const { cadastrarUsuario, login, getDadosUsuario, getListarCategorias, listarTransacoes, detalharTransacao, editarUsuario } = require("./controllers/usuarios");
 const { validarLogin } = require("./validarLogin");
 const validarToken = require("./validarToken");
 const rotas = express();
@@ -8,6 +8,7 @@ const rotas = express();
 rotas.post("/usuario", cadastrarUsuario);
 rotas.post("/login", validarLogin, login);
 rotas.get("/usuario", validarToken, getDadosUsuario)
+rotas.put("/usuario", validarToken, editarUsuario)
 rotas.get("/categoria", validarToken, getListarCategorias)
 rotas.get("/transacao", validarToken, listarTransacoes)
 rotas.get("/transacao/:id", validarToken, detalharTransacao)
