54ca0c658a6a515e82dacef922ee886ee5be5154
kyomi <104471832+kyomi-dev@users.noreply.github.com>
2023-11-22 09:44:41 -0300

    refactor(database): Remove a inserção de valores na tabela 'Categorias'

--- src/tabelas.sql
+++ src/tabelas.sql
@@ -22,7 +22,3 @@ CREATE TABLE
         usuario_id INTEGER REFERENCES usuarios(id),
         tipo TEXT NOT NULL
     );
-
-INSERT INTO
-    categorias (descricao)
-VALUES ('Alimentação'), ('Assinatura e Serviços'), ('Casa'), ('Mercado'), ('Cuidados Pessoais'), ('Educação'), ('Família'), ('Lazer'), ('Pets'), ('Presentes'), ('Roupas'), ('Saúde'), ('Transporte'), ('Salário'), ('Vendas'), ('Outras receitas'), ('Outras despesas');
